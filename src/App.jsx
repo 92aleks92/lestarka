e3e9f8a7bf3b7ed377ce08a1b326e3e92e26d65b
92aleks92 <xiaomi8mi8@gmail.com>
2023-04-18 20:14:19 +0200

    project start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="root">
+      <header>
+        <Header />
+      </header>
+    </div>
+  );
+}
+
+export default App;
